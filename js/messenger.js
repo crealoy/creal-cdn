ab8d9c3748307296288da9b9cc1f55c35dea6959
Juho Vähä-Herttua <juhovh@iki.fi>
2017-10-01 22:24:13 +0300

    Update messenger initialisation code to latest version.

--- js/messenger.js
+++ js/messenger.js
@@ -1,21 +1,48 @@
+/*jslint
+    for
+*/
 /*global
     window, document, fetch, MessengerExtensions, me,
-    crealAppId, crealApiBaseUrl, crealAppPrefix
+    crealAppId, crealAppPrefix, crealApiBaseUrl, crealSiteBaseUrl,
+    crealAppErrorFn, crealAppProfileRequestFn, crealAppMessagingRequestFn
 */
 
 (function () {
     "use strict";
 
-    function showError(code, message) {
-        // First parameter is the error code if available
-        var errorCodeElement = document.getElementById('errorCode');
-        var errorMessageElement = document.getElementById('errorMessage');
-
-        if (code && errorCodeElement) {
-            errorCodeElement.textContent = code;
+    function getQueryParameter(key) {
+        var query = window.location.search.substr(1);
+        var params = query.split('&');
+        var i, idx;
+        for (i = 0; i < params.length; i += 1) {
+            idx = params[i].indexOf('=');
+            if (idx >= 0) {
+                if (key === decodeURIComponent(params[i].substr(0, idx))) {
+                    return decodeURIComponent(params[i].substr(idx + 1));
+                }
+            }
         }
-        if (message && errorMessageElement) {
-            errorMessageElement.textContent = message;
+    }
+
+    function showError(code, message) {
+        if (typeof crealAppErrorFn === "function") {
+            crealAppErrorFn(code, message);
+        }
+    }
+
+    function showProfileRequest(callback) {
+        if (typeof crealAppProfileRequestFn === "function") {
+            crealAppProfileRequestFn(callback);
+        } else {
+            callback();
+        }
+    }
+
+    function showMessagingRequest(callback) {
+        if (typeof crealAppMessagingRequestFn === "function") {
+            crealAppMessagingRequestFn(callback);
+        } else {
+            callback();
         }
     }
 
@@ -28,22 +55,49 @@
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({signedContext: signedContext})
+            body: JSON.stringify({
+                signedContext: signedContext,
+                shareToken: getQueryParameter('shareToken')
+            })
         }).then(function (res) {
-            // FIXME: Should check status code
-            return res.json();
+            // This could be improved for better errors
+            if (!res.ok) {
+                throw new Error('Error ' + res.status + ' on login');
+            } else {
+                return res.json();
+            }
         }).then(function (res) {
             me.creal.frontend.core.init({
                 authResponse: res.authResponse,
                 userContext: res.userContext,
+                apiBaseUrl: crealApiBaseUrl,
+                baseUrl: crealSiteBaseUrl,
                 prefix: crealAppPrefix
             });
         }).catch(function (error) {
-            showError(null, JSON.stringify(error));
+            showError(null, error + ' - ' + JSON.stringify(error));
         });
     }
 
-    window.extAsyncInit = function () {
+    var askedPermissions = [];
+    function askPermission(name, callback) {
+        askedPermissions(name);
+        MessengerExtensions.askPermission(function (response) {
+            callback(response.permissions);
+        }, function (code, message) {
+            showError(code, message);
+        }, name);
+    }
+
+    function getPermissions(callback) {
+        MessengerExtensions.getGrantedPermissions(function (response) {
+            callback(response.permissions);
+        }, function (code, message) {
+            showError(code, message);
+        });
+    }
+
+    function handleLogin() {
         // The Messenger Extensions JS SDK is done loading
         MessengerExtensions.getSupportedFeatures(function (result) {
             var features = result.supported_features;
@@ -59,5 +113,34 @@
         }, function (code, message) {
             showError(code, message);
         });
+    }
+
+    window.extAsyncInit = function () {
+        function permissionsCallback(permissions) {
+            if (permissions.indexOf('user_profile') < 0) {
+                if (askedPermissions.indexOf('user_profile') < 0) {
+                    // On first time ask permission directly from user
+                    askPermission('user_profile', permissionsCallback);
+                } else {
+                    // If permission was denied, ask more politely
+                    showProfileRequest(function () {
+                        askPermission('user_profile', permissionsCallback);
+                    });
+                }
+            } else if (permissions.indexOf('user_messaging') < 0) {
+                if (askedPermissions.indexOf('user_messaging') < 0) {
+                    // On first time ask permission directly from user
+                    askPermission('user_messaging', permissionsCallback);
+                } else {
+                    // If permission was denied, ask more politely
+                    showMessagingRequest(function () {
+                        askPermission('user_messaging', permissionsCallback);
+                    });
+                }
+            } else {
+                handleLogin();
+            }
+        }
+        getPermissions(permissionsCallback);
     };
 }());
